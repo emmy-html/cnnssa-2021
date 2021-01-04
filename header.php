67745f135a66e5c9fe40d01dd117c3070bac4c86
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-01-04 10:37:21 -0600

    Initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<section id="suggestion-box-popover">
+      <div class="suggestion-box-popover-content fade-in-up sticky">
+          <a href="/suggestion-box">Suggestion Box</a>
+          <button onclick="closeSuggestionPopover()">X</button>
+      </div>
+</section>
+<header id="header-nav">
+<img src="<?php echo get_stylesheet_directory_uri();?>/img/cnnssa_logo.png" width="252" height="50">
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+</header>
+<div id="translate-option" class="translate-section">
+  <?php echo do_shortcode('[prisna-google-website-translator]'); ?>
+</div>
+<body id="body-adjust-main" <?php body_class(); ?>>
+<div id="container">
